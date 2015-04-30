70bd5e18cdd8feb571f50a298640a90b3dfce47e
Stonyx <info@stonyx.com>
2015-04-30 17:52:54 -0400

    Added HTML/CSS/JS code for the user interface.

--- SD Card Files/index.js
+++ SD Card Files/index.js
@@ -1,4 +1,31 @@
 // Run this when ready
 $(document).ready(function() 
 {
+  // Create the AJAX request object
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'get?st', true);
+  xhr.responseType = 'arraybuffer';
+  xhr.onload = function(event)
+  {
+    // Check if we received an OK response
+    if (this.status == 200)
+    {
+      // Get the time and the color values data
+      var time = new Uint32Array(this.response).subarray(0, 1);
+      var values = new Uint8Array(this.response).subarray(4);
+
+      // Create a date object
+      var date = new Date(time[0] * 1000);
+
+      // Display the time and color values      
+      $("#time").html();
+      $("#red").html(values[0]);
+      $("#green").html(values[1]);
+      $("#blue").html(values[2]);
+      $("#white").html(values[3]));
+    }
+  };
+
+  // Send the AJAX request
+  xhr.send();
 });
